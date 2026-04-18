d894c5fcc85ea450a8ab76f52cb489b801deac1b
Alfredo Mamby Bossa <alfredo.mamby@aossas.com>
2026-04-18 17:46:59 -0500

    CorrecciÃon de variables de entorno

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -1,29 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   private apiUrl = '/api/users';
+  private authorizaUrl = environment.BASE_URL_AUTHORIZA;
 
-  // Crear un BehaviorSubject para almacenar los usuarios
   private usersSubject = new BehaviorSubject<any[]>([]);
   public users$ = this.usersSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
-  // Método para obtener los usuarios desde el servidor
   getUsers(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
 
-  // Método para cargar los usuarios y actualizar el BehaviorSubject
   loadUsers(): void {
     this.getUsers().subscribe({
       next: (users) => {
-        this.usersSubject.next(users); // Actualiza el BehaviorSubject
+        this.usersSubject.next(users);
       },
       error: (error) => {
         console.error('Error al cargar usuarios', error);
@@ -31,7 +30,6 @@ export class UserService {
     });
   }
 
-  // Método para acceder a los usuarios actuales
   getCurrentUsers(): any[] {
     return this.usersSubject.getValue();
   }
@@ -41,7 +39,7 @@ export class UserService {
     oldPassword: string,
     newPassword: string
   ): Observable<any> {
-    return this.http.post(`/api/users/${userId}/change-password`, {
+    return this.http.post(`${this.authorizaUrl}/users/${userId}/change-password`, {
       oldPassword,
       newPassword,
     });
