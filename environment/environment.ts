c4d9b5987b317ae864262e938832b94478c54d8d
Alfredo Mamby Bossa <alfredo.mamby@aossas.com>
2026-04-20 11:08:53 -0500

    Leyenda en vista login

--- environment/environment.ts
+++ environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    BASE_URL_AUTHORIZA: 'http://localhost:3000/api', // URL del backend de Authoriza
+    BASE_URL_AUTHORIZA: 'http://localhost:3000', // URL del backend de Authoriza
     BASE_URL_INOUT: 'http://localhost:3001', // URL del backend de Inout
     apiUrl: 'http://localhost:3001/api' // URL base para APIs
   };
